66bb567426eb5b905baa7c22692754d19424495f
Maxim Torgonsky <kriolog@gmail.com>
2016-09-26 19:47:27 +0200

    Add restoring of the chat content after reconnect.

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var db = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  io.emit('chat_message', 'user connected'.fontcolor("green"));
+  const user_connected = 'user connected'.fontcolor("green");
+  db.push(user_connected);
+  socket.emit('chat_message', db.join('<br>'));
+  socket.broadcast.emit('chat_message', user_connected);
 
   socket.on('chat_message', function(msg){
+    db.push(msg);
     socket.broadcast.emit('chat_message', msg);
   });
 
   socket.on('disconnect', function(){
-    socket.broadcast.emit('chat_message', 'user disconnected'.fontcolor("red"));
+    const user_disconnected = 'user disconnected'.fontcolor("red");
+    db.push(user_disconnected);
+    socket.broadcast.emit('chat_message', user_disconnected);
   });
 });
 
